a45c9e5f86642d6d206295edcf4e30b633d631af
b4tchkn <baaaakkrad@gmail.com>
2025-01-25 14:01:59 +0900

    fix: remove Greeting

--- app/src/main/kotlin/io/github/b4tchkn/timez/MainActivity.kt
+++ app/src/main/kotlin/io/github/b4tchkn/timez/MainActivity.kt
@@ -5,11 +5,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import io.github.b4tchkn.timez.ui.theme.TimezTheme
 
 class MainActivity : ComponentActivity() {
     @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
@@ -19,24 +14,5 @@ class MainActivity : ComponentActivity() {
         setContent {
             TimezApp()
         }
-    }
-}
-
-@Composable
-fun Greeting(
-    name: String,
-    modifier: Modifier = Modifier,
-) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier,
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TimezTheme {
-        Greeting("Android")
     }
 }
