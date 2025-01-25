2f43b77c7f640d2544cdab56d3009fddb23cb94a
Kakeru Nakabachi <baaaakkrad@gmail.com>
2025-01-25 16:21:48 +0900

    feat: install hilt (#9)

--- app/src/main/kotlin/io/github/b4tchkn/timez/data/NewsApiService.kt
+++ app/src/main/kotlin/io/github/b4tchkn/timez/data/NewsApiService.kt
@@ -1,20 +1,14 @@
 package io.github.b4tchkn.timez.data
 
-import io.github.b4tchkn.timez.data.network.AuthHeaderInterceptor
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 import io.github.b4tchkn.timez.model.News
-import kotlinx.serialization.json.Json
-import okhttp3.MediaType.Companion.toMediaType
-import okhttp3.OkHttpClient
 import retrofit2.Retrofit
-import retrofit2.converter.kotlinx.serialization.asConverterFactory
+import retrofit2.create
 import retrofit2.http.GET
 import retrofit2.http.Query
-
-object NewsApi {
-    val service: NewsApiService by lazy {
-        retrofit.create(NewsApiService::class.java)
-    }
-}
 
 interface NewsApiService {
     @GET("top-headlines")
@@ -23,23 +17,9 @@ interface NewsApiService {
     ): News
 }
 
-private const val BASE_URL = "https://newsapi.org/v2/"
-
-private val json =
-    Json {
-        ignoreUnknownKeys = true
-    }
-
-private val retrofit =
-    Retrofit
-        .Builder()
-        .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
-        .baseUrl(BASE_URL)
-        .client(okHttpClient())
-        .build()
-
-private fun okHttpClient() =
-    OkHttpClient
-        .Builder()
-        .addInterceptor(AuthHeaderInterceptor())
-        .build()
+@Module
+@InstallIn(SingletonComponent::class)
+object NewsApiServiceModule {
+    @Provides
+    fun provide(retrofit: Retrofit): NewsApiService = retrofit.create()
+}
