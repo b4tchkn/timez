1f1e3bfaaae5f34166b3957366ff892c4a2bbb62
b4tchkn <baaaakkrad@gmail.com>
2025-01-28 19:04:01 +0900

    feat: add loading box

---
+++ app/src/main/kotlin/io/github/b4tchkn/timez/ui/component/LoadingBox.kt
@@ -0,0 +1,40 @@
+package io.github.b4tchkn.timez.ui.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun LoadingBox(
+    modifier: Modifier,
+    loading: Boolean,
+    content: @Composable () -> Unit,
+) {
+    Box(
+        modifier = modifier
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center,
+    ) {
+        content()
+        if (loading) {
+            Box(
+                modifier = Modifier
+                    .size(80.dp)
+                    .clip(RoundedCornerShape(8.dp))
+                    .background(color = Color.LightGray),
+                contentAlignment = Alignment.Center,
+            ) {
+                CircularProgressIndicator()
+            }
+        }
+    }
+}
