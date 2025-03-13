854147fd0f79017746cc1267b1e07fe4b4ea3ad7
tkhskt <luckymortality@gmail.com>
2025-03-14 00:04:20 +0900

    fix: lint

--- app/src/main/kotlin/io/github/b4tchkn/timez/core/ResultExt.kt
+++ app/src/main/kotlin/io/github/b4tchkn/timez/core/ResultExt.kt
@@ -2,10 +2,6 @@ package io.github.b4tchkn.timez.core
 
 import kotlin.coroutines.cancellation.CancellationException
 
-inline fun <reified E : Throwable, T> Result<T>.onFailureOrRethrow(action: (Throwable) -> Unit): Result<T> {
-    return onFailure { if (it is E) throw it else action(it) }
-}
+inline fun <reified E : Throwable, T> Result<T>.onFailureOrRethrow(action: (Throwable) -> Unit): Result<T> = onFailure { if (it is E) throw it else action(it) }
 
-inline fun <T> Result<T>.onFailureIgnoreCancellation(action: (Throwable) -> Unit): Result<T> {
-    return onFailureOrRethrow<CancellationException, T>(action)
-}
+inline fun <T> Result<T>.onFailureIgnoreCancellation(action: (Throwable) -> Unit): Result<T> = onFailureOrRethrow<CancellationException, T>(action)
