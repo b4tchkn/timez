efa093c5bab13870ad4de634ead40d8c8d1f18ad
Kakeru Nakabachi <baaaakkrad@gmail.com>
2025-03-21 14:02:54 +0900

    chore: compose lint (#64)

--- app/src/main/kotlin/io/github/b4tchkn/timez/data/repository/DefaultNewsRepository.kt
+++ app/src/main/kotlin/io/github/b4tchkn/timez/data/repository/DefaultNewsRepository.kt
@@ -2,13 +2,15 @@ package io.github.b4tchkn.timez.data.repository
 
 import io.github.b4tchkn.timez.model.Article
 import io.github.b4tchkn.timez.network.NewsDataSource
+import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.toImmutableList
 import javax.inject.Inject
 
 class DefaultNewsRepository @Inject constructor(
     private val newsDataSource: NewsDataSource,
 ) : NewsRepository {
-    override suspend fun topHeadlines(): List<Article> {
+    override suspend fun topHeadlines(): ImmutableList<Article> {
         val headlines = newsDataSource.getTopHeadlines(country = "us").articles
-        return headlines
+        return headlines.toImmutableList()
     }
 }
--- app/src/test/kotlin/io/github/b4tchkn/timez/fake/FakeNewsRepository.kt
+++ app/src/test/kotlin/io/github/b4tchkn/timez/fake/FakeNewsRepository.kt
@@ -3,17 +3,19 @@ package io.github.b4tchkn.timez.fake
 import app.cash.turbine.Turbine
 import io.github.b4tchkn.timez.data.repository.NewsRepository
 import io.github.b4tchkn.timez.model.Article
+import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.toImmutableList
 
 class FakeNewsRepository(
     private val shouldFail: Boolean = false,
 ) : NewsRepository {
     val articles = Turbine<List<Article>>()
 
-    override suspend fun topHeadlines(): List<Article> {
+    override suspend fun topHeadlines(): ImmutableList<Article> {
         if (shouldFail) {
             throw fakeNewsRepositoryException
         } else {
-            return articles.awaitItem()
+            return articles.awaitItem().toImmutableList()
         }
     }
 }
