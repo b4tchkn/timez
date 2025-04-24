4323d97558b03117ecb2cd513baed27c3ead71d6
b4tchkn <baaaakkrad@gmail.com>
2025-04-24 22:33:48 +0900

    chore: update existing comment

--- .github/scripts/report-vrt.js
+++ .github/scripts/report-vrt.js
@@ -13,12 +13,14 @@ module.exports = async ({ github, context, core }) => {
   };
 
   const noDiff = (stats.changed === "0" && stats.newItems === "0" && stats.deleted === "0");
+  const endLineMessage = "This is result of vrt."
 
   let body;
   if (noDiff) {
     body = await core.summary
         .addRaw("**✨✨ That's perfect, there is no visual difference! ✨✨**", true)
-        .addRaw(`🔵 Passing: ${stats.passing}`)
+        .addRaw(`🔵 Passing: ${stats.passing}`, true)
+        .addRaw(endLineMessage, true)
         .stringify();
   } else {
     body = await core.summary
@@ -27,15 +29,54 @@ module.exports = async ({ github, context, core }) => {
           ["🔴 Changed",  "⚪️ New",       "⚫️ Deleted",  "🔵 Passing"],
           [stats.changed, stats.newItems, stats.deleted, stats.passing]
         ])
-        .addHeading("📊 Download Report", 3)
+        .addRaw("**📊 Download Report**", true)
         .addLink("View Report", "https://www.youtube.com/")
-        .stringify()
+        .addBreak()
+        .addRaw(endLineMessage, true)
+        .stringify();
   }
 
-  github.rest.issues.createComment({
-    issue_number: context.issue.number,
-    owner: context.repo.owner,
-    repo: context.repo.repo,
-    body: body,
-  });
+  const existingCommentId = await findExistingCommentId(github, context, endLineMessage);
+
+  if (existingCommentId) {
+    github.rest.issues.updateComment({
+        comment_id: existingCommentId,
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        body: body,
+     });
+  } else {
+    github.rest.issues.createComment({
+        issue_number: context.issue.number,
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        body: body,
+    });
+  }
+}
+
+async function findExistingCommentId(github, context, identifier) {
+  const owner = context.repo.owner;
+  const repo = context.repo.repo;
+  const issue_number = context.issue.number;
+  let commentIdToUpdate = null;
+
+  try {
+    const { data: comments } = await github.rest.issues.listComments({
+      owner,
+      repo,
+      issue_number,
+    });
+
+    for (const comment of comments) {
+      if (comment.user.type === "Bot" && comment.body.includes(identifier)) {
+        commentIdToUpdate = comment.id;
+        break;
+      }
+    }
+  } catch (error) {
+    return null;
+  }
+
+  return commentIdToUpdate;
 }
