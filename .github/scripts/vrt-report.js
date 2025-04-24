18777e1189e4a86105e78e835d74af61ec5f513b
b4tchkn <baaaakkrad@gmail.com>
2025-04-24 23:57:03 +0900

    chore: debug

--- .github/scripts/vrt-report.js
+++ .github/scripts/vrt-report.js
@@ -1,10 +1,16 @@
 module.exports = async ({ github, context, core }) => {
-  const passing = 10
+  const passing = 10;
+  const endLineMessage = "This is result of vrt.";
   const markdown = await core.summary
-    .addRaw("**✨✨ That's perfect, there is no visual difference! ✨✨**", true)
-    .addRaw(`🔵 Passing: ${passing}`)
-    .addLink("View Report", "https://www.youtube.com/")
-    .stringify();
+        .addRaw("**Detected visual differences**")
+        .addTable([
+          ["🔴 Changed",  "⚪️ New",       "⚫️ Deleted",  "🔵 Passing"],
+          ["10", "10", "0", "10"]
+        ])
+        .addLink("View Report", "https://www.youtube.com/")
+        .addBreak()
+        .addRaw(endLineMessage)
+        .stringify();
 
   github.rest.issues.createComment({
     issue_number: context.issue.number,
