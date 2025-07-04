c475dbfb37360aca853bcc0b84cabb5ec46ff30e
Kakeru Nakabachi <baaaakkrad@gmail.com>
2025-07-05 00:27:41 +0900

    fix: Resolve deprecation warnings (#149)

--- app/src/main/kotlin/io/github/b4tchkn/timez/core/NowLocalDateTime.kt
+++ app/src/main/kotlin/io/github/b4tchkn/timez/core/NowLocalDateTime.kt
@@ -1,7 +1,7 @@
 package io.github.b4tchkn.timez.core
 
 import androidx.compose.runtime.staticCompositionLocalOf
-import kotlinx.datetime.Clock
+import kotlin.time.Clock
 import kotlinx.datetime.LocalDateTime
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.toLocalDateTime
@@ -12,6 +12,7 @@ interface NowLocalDateTime {
 
 object DefaultNowLocalDateTime : NowLocalDateTime {
     override val value: LocalDateTime
+        @OptIn(kotlin.time.ExperimentalTime::class)
         get() = Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault())
 }
 
